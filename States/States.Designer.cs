4696d28ba1dc0e2c589e2247ce7975607848fafd
wangt47 <60717538+wangt47@users.noreply.github.com>
2020-02-05 15:31:05 -0800

    Add project files.

---
+++ States/States.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace States
+{
+    partial class States
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(25, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a State";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(25, 100);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(158, 148);
+            this.listBox1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.IntegralHeight = false;
+            this.comboBox1.Location = new System.Drawing.Point(25, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(158, 24);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectionChangeCommitted += new System.EventHandler(this.ComboBox1_SelectionChangeCommitted);
+            // 
+            // States
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(208, 290);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "States";
+            this.Text = "States";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ States/States.cs
@@ -0,0 +1,90 @@
+﻿/*
+ CISP 405 Summer 2019 Tony Wang SID # 1443145
+ 
+ Assignment 3 Description:
+ Create another GUI but with advanced techniques
+ such as learn how to create an Event Handler
+ method and execute this method to an event from 
+ a GUI component (i.e. listbox, combobox, etc.)
+ and select the appropriate event for the 
+ Event Handler method to perform its intended task.
+
+ States.cs
+ The main C# file to house the Windows Form App's code.
+ This program fulfills the assignment's purpose:
+ Have the user to click select a state from combobox and
+ then the selected state transfers itself to listbox after
+ selection. Repeat until the last state from the combobox 
+ is selected and added last to the listbox, and then a 
+ Message Box pops up to notify the user that no states are
+ left from the combobox and the program closes itself after
+ closing the Message Box.
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace States
+{
+    public partial class States : Form
+    {
+        // an int counter for keeping track of number of states
+        // currently left in comboBox1.
+        private int numOfItems;
+
+        // constructor
+        public States()
+        {
+            // Windows Form is initialized.
+            InitializeComponent();
+
+           /* Originally put the state names in the comboBox1 collection 
+              but I decided to use the string array provided just to prove
+              the use of implementation of state names in code.*/
+           string[] statesList = { "New Jersey", "Idaho", "Illinois",
+         "Indiana", "Iowa", "Kansas", "Kentucky", "Maryland",
+         "Massachusetts", "Michigan", "Wisconsin", "Wyoming",
+         "Oregon", "Pennsylvania", "Rhode Island" };
+
+            // comboBox1 gets all states from the stateslist string array.
+            comboBox1.Items.AddRange(statesList);
+
+            // numOfItems gets the total count of 15 states from the 
+            // comboBox1 count of items.
+            numOfItems = comboBox1.Items.Count;
+        }
+
+        // handles comboBox1's SelectionChangeCommitted event
+        private void ComboBox1_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            // The selected item from comboBox1 is added to listBox1.
+            listBox1.Items.Add(comboBox1.SelectedItem);
+
+            // The selected item from comboBox1 is removed for immediate selection.
+            comboBox1.Items.Remove(comboBox1.SelectedItem);
+
+            // int counter for states subtracts by one after the selection.
+            numOfItems--;
+
+            // check if the int counter reaches to zero
+            if (numOfItems == 0)
+            {
+                /* that comboBox1 currently holds 0 states and then a
+                   Message Box pop-up to let user know that there are no states
+                   left to transfer from comboBox1 to listBox1 and the user is 
+                   prompted to press OK to continue. */
+                MessageBox.Show("There are no more states in the ComboBox.", "Empty", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                // App closes itself after clicking OK on the Message Box pop-up.
+                Application.Exit();
+            }
+        }
+    }
+}
+
